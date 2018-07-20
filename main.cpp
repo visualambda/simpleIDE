21fb60c4db93a7ae4ced8ae426258cff5473fb2c
zhu-fei <jellyzone@gmail.com>
2018-07-20 11:58:17 +0800

    add click lambda

--- main.cpp
+++ main.cpp
@@ -3,12 +3,22 @@
 #include <folderView/multifolderview.h>
 #include <folderView/itemdelegate.h>
 #include <QVBoxLayout>
+#include <QMessageBox>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
     MultiFolderView *mfv = new MultiFolderView(&w);
+    QObject::connect(
+                mfv,
+                &MultiFolderView::clicked /*SIGNAL( clicked( QModelIndex ) )*/,
+                [=] (QModelIndex i) -> void
+                    {
+                        QMessageBox::question(0, QString::number(i.row()), QString::number(i.column()), QMessageBox::Question);
+                    }
+    );
+
 
     ItemDelegate * id = new ItemDelegate();
     mfv->setItemDelegate(id);
